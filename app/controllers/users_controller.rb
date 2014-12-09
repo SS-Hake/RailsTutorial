b8025efcbda1c8bbe4d65e3de8d8f962411337af
Sam Scott-Hake <sam_hake@live.co.uk>
2014-12-09 00:11:54 +0000

    added sign up error messages & display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      #This returns true if it can save successfully - handle the save here
+    else
+      render 'new'
+    end
+  end
+  
+  #These methods are only used internally and as such can be 'private'
+  private
+    #This method ensures that publlic users have access only to the parameters of @user we want them
+    #to have - disallows users to set thier own admin priviledges.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
