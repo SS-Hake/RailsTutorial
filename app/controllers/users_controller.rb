ffaf97ccf291a0ccb299727edff72d6f7c09f624
Sam Scott-Hake <sam_hake@live.co.uk>
2014-12-09 01:09:50 +0000

    Finish user signup. Working flash on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #This returns true if it can save successfully - handle the save here
+      flash[:success] = "Welcome to Chirp!"
+      redirect_to @user
     else
       render 'new'
     end
